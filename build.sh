4f6e5043604e8c0e78d5af7d4d0e46c126c05f16
Zbigniew Polito <zbigniew.polito@outlook.com>
2023-11-08 10:39:20 +0100

    build failed

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
-echo "Build step test"
+
+mkdir -p build
+
+touch build/build_no
+build_no=$(cat build/build_no)
+
+if ! [[ "$build_no" =~ ^[0-9]+$ ]]
+then
+  build_no=0
+fi;
+
+build_no=$((build_no+1))
+
+echo "$build_no" > build/build_no;
+echo "Build number $build_no"
+
+exit 1
